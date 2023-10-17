58325320378787d30722086403d8a403287ca1d3
oladele sunday <oladelesundayjoshua@gmail.com>
2023-10-17 06:02:42 -0700

    This is Third trial due to betty style

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,7 +3,6 @@
  * Auth: Brennan D Baraban
  */
 
-
 #include <stdio.h>
 
 /**
@@ -14,13 +13,12 @@
  */
 int main(void)
 {
-    int num;
+	int num;
 
-    for (num = 0; num < 10; num++)
-        putchar(num + '0');
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
 
-    putchar('\n');
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
-
